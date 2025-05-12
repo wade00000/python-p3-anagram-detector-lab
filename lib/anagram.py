2228d455f6ec6c49bb86353d9648bfdbd24f4938
WADE <MEANTOINE12@GMAIL.COM>
2025-05-12 13:42:03 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram():
+    def __init__(self,word):
+        self.word = word.lower()
+
+    def match(self,word_list):
+        sorted_word = sorted(self.word)
+        matches = []
+
+        for term in word_list:
+            if term.lower() != self.word and sorted(term) == sorted_word:
+                matches.append(term)
+
+        return matches
+    
+beast = Anagram("beast")
+print(beast.match(["abets","baste","betas","beast","beats"]))
+
+        
+        
